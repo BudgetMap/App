910bdef942ca6243fc10081a9900c1fa51518bac
BudgetMap <budgetmapapp@outlook.com>
2024-10-28 15:46:52 +0300

    fixed double pop

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar({required BuildContext context, required String title}) {
+  return AppBar(
+    backgroundColor: Theme.of(context).colorScheme.surface,
+    title: Text(
+      title,
+      style: TextStyle(
+          color: Theme.of(context).colorScheme.onSurfaceVariant,
+          fontFamily: Theme.of(context).textTheme.displaySmall?.fontFamily,
+          fontSize: Theme.of(context).textTheme.displaySmall?.fontSize),
+    ),
+    toolbarHeight: 80,
+  );
+}
